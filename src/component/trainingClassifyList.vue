f15b90b860e0414894fa6831ab214489d5f5f825
Drama <abandoneddrama@foxmail.com>
2017-04-10 11:12:47 +0800

    修改培训分类删除时的提示

--- src/component/trainingClassifyList.vue
+++ src/component/trainingClassifyList.vue
@@ -145,7 +145,7 @@
 
       // 删除
       deleteTrain (id) {
-        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        this.$confirm('此操作将永久删除该分类下所有培训内容, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning'
