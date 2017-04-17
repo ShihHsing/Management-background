5401bb74f16fd342c12178a8bed78c594d05b49e
Drama <abandoneddrama@foxmail.com>
2017-04-17 11:27:07 +0800

    修改上线IP

--- src/component/logoTop.vue
+++ src/component/logoTop.vue
@@ -3,7 +3,7 @@
     <el-col :span="12" class="logoTitle">
       <router-link to="/home"><h1>哎哟不错店家管理系统(master版)</h1></router-link>
     </el-col>
-    <el-col :span="3" :offset="7" class="storeSelect">
+    <!-- <el-col :span="3" :offset="7" class="storeSelect">
       <el-select v-model="value" placeholder="请选择">
         <el-option
           v-for="item in options"
@@ -11,7 +11,7 @@
           :value="item.value">
         </el-option>
       </el-select>
-    </el-col>
+    </el-col> -->
     <el-col :span="2" class="user">
       <div>
         <img :src="userImgUrl" :alt="userName" @click="dialogVisible = true">
@@ -59,10 +59,8 @@ export default {
         label: '石岩店'
       }],
       value: '选项1',
-      // 用户img
-      userImgUrl: 'http://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1484901235476&di=0b91b607a22beca9c92ce65a9c64084f&imgtype=0&src=http%3A%2F%2Fb.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Ff603918fa0ec08faf0f7ace15cee3d6d54fbda85.jpg',
       // 用户名字
-      userName: '石鑫',
+      userName: '',
       userPhone: store.state.user.userData.phone_number
       // session_id: store.state.user.userData.session_id
     }
