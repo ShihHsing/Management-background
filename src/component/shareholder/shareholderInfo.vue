eb8116d0854fb38f57f0f948cd4ea3481313d2fc
Drama <abandoneddrama@foxmail.com>
2017-08-02 14:06:50 +0800

    fix(loation): 股东详情页 积分变动明细 取消分页展示

--- src/component/shareholder/shareholderInfo.vue
+++ src/component/shareholder/shareholderInfo.vue
@@ -79,7 +79,7 @@
                             label="变动后剩余积分">
                         </el-table-column>
                     </el-table>
-                    <div class="pagination">
+                    <!-- <div class="pagination">
                         <el-pagination
                             @current-change="handleCurrentChange"
                             :current-page="current_page"
@@ -88,7 +88,7 @@
                             :total="total"
                             style="float: right;padding: 0;">
                         </el-pagination>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -281,9 +281,14 @@ export default {
         }
     },
     created: function () {
+        // 获取股东详情
         this.getShareholderInfoById()
     },
     methods: {
+        // 下一页
+        handleCurrentChange () {
+
+        },
         // 获取股东详情
         getShareholderInfoById () {
             // 重置表单
